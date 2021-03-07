dc9edab2fc237c2171a5479187233130bef14509
viniciusad <viniciusad@outlook.com.br>
2021-03-07 20:54:40 -0300

    Estrutura base da página

---
+++ js/script.js
@@ -0,0 +1,23 @@
+/* ============================== SCROLL TOP ============================== */
+// Criando Botão
+var mybutton = document.getElementById("back-top");
+            
+// Definindo scroll para aparecer o botão
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.visibility = "visible";
+  } else {
+    mybutton.style.visibility = "hidden";
+  }
+}
+
+// Ao clicar vai para o topo da página
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+/* ============================== SCROLL TOP ============================== */
+var mybutton = document.getElementById("linkmap");
