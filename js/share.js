4ebe6495d90486f41e71141af93e7ec5b52ea8e0
viniciusad <viniciusad@outlook.com.br>
2021-03-18 18:46:02 -0300

    Add pag Noticias

--- js/share.js
+++ js/share.js
@@ -1,10 +1,10 @@
-let url = window.location;
+let url = window.location.href;
 let title = document.title;
 let mnsocial = document.getElementsByClassName('mn-social-bottom');
 mnsocial[0].href = 'https://www.facebook.com/sharer/sharer.php?u=' + url;
-mnsocial[1].href = 'https://twitter.com/home?status=' + url;
-mnsocial[2].href = 'https://www.linkedin.com/shareArticle?mini=true&url=' + url;
+mnsocial[1].href = 'https://twitter.com/intent/tweet?text=' + 'Olha que legal esse site: ' + title + ' ' + url;
+mnsocial[2].href = 'https://www.linkedin.com/cws/share?url=' + url;
 mnsocial[3].href = 'mailto:?&subject=' + url + '&body=' + title + ', ' + url;
 mnsocial[4].href = 'https://api.whatsapp.com/send?text=' + url;
-// mnsocial[6].href = 'http://www.reddit.com/submit?url=' + url;
-// mnsocial[7].href = 'https://pinterest.com/pin/create/link/?url=' + url;
+// mnsocial[5].href = 'http://www.reddit.com/submit?url=' + url;
+// mnsocial[6].href = 'https://pinterest.com/pin/create/link/?url=' + url;
